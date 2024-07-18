625e87498186b2a7fa86466f2938fda20b6de4df
ParkMinjun0721 <83351998+ParkMinjun0721@users.noreply.github.com>
2024-07-18 09:52:18 +0900

    Restore

---
+++ lib/map/store_list.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/theme/font.dart';
+import 'package:test_project/components/my_navigationbar.dart';
+
+class StoreListPage extends StatelessWidget {
+  const StoreListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('매장목록', style: pretendardRegular(context)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: 359,
+              height: 36,
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // 첫 번째 아이콘과 텍스트
+                        Container(
+                          height: 36,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 20,
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(width: 1, color: Color(0xFFD3D5DD)),
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
+                              ),
+                              const SizedBox(width: 9),
+                              Text(
+                                '판매중',
+                                style: pretendardRegular(context).copyWith(
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // 두 번째 정렬 아이콘과 버튼
+                        Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 83,
+                                height: 36,
+                                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(width: 1, color: Color(0xFFEDF1F7)),
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.sort),
+                                    const SizedBox(width: 5),
+                                    Text(
+                                      '정렬',
+                                      style: pretendardRegular(context).copyWith(
+                                        fontSize: 14,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 4),
+                              Container(
+                                width: 36,
+                                height: 36,
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(width: 1, color: Color(0xFFEDF1F7)),
+                                  borderRadius: BorderRadius.circular(24),
+                                ),
+                                child: Icon(Icons.search),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const CustomNavigationBar(),
+    );
+  }
+}
---
+++ lib/storedetail/store_detail.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/theme/font.dart';
+import 'package:test_project/components/my_navigationbar.dart';
+import '../components/my_divider.dart';
+
+class StoreDetailPage extends StatelessWidget {
+  const StoreDetailPage({Key? key, required this.name}) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1634560604992-7784a29bc419?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back_ios_new_outlined, color: Theme.of(context).colorScheme.surface),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.favorite_border, color: Theme.of(context).colorScheme.surface),
+                            SizedBox(width: 10),
+                            Icon(Icons.shopping_cart_outlined, color: Theme.of(context).colorScheme.surface),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.surface,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '포항시 북구 양덕동',
+                            style: pretendardMedium(context).copyWith(
+                              fontSize: 14,
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '양덕동 마카롱',
+                            style: pretendardSemiBold(context).copyWith(
+                              fontSize: 20,
+                              color: Theme.of(context).colorScheme.onSurface,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Icon(Icons.star_rounded, color: Colors.yellow[700], size: 16),
+                              SizedBox(width: 5),
+                              Text(
+                                '5.0',
+                                style: pretendardSemiBold(context).copyWith(
+                                  fontSize: 14,
+                                  color: Theme.of(context).colorScheme.onSurface,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                '/ 116개의 리뷰',
+                                style: pretendardMedium(context).copyWith(
+                                  fontSize: 14,
+                                  color: Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          SmallDivider(),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(Icons.access_time_rounded, color: Theme.of(context).colorScheme.secondary),
+                              SizedBox(width: 6),
+                              Text(
+                                '영업중',
+                                style: pretendardMedium(context).copyWith(
+                                  fontSize: 14,
+                                  color: Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                '오늘 09:30 ~ 20:00',
+                                style: pretendardMedium(context).copyWith(
+                                  fontSize: 14,
+                                  color: Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const CustomNavigationBar(),
+    );
+  }
+}
