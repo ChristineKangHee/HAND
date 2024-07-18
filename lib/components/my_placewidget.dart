d4f1ec9e34066834e729f55ee536782345e146a7
kanghee <christinekangh522@gmail.com>
2024-07-18 10:20:02 +0900

    model 추가

--- lib/components/my_placewidget.dart
+++ lib/components/my_placewidget.dart
@@ -1,31 +1,15 @@
+// my_placewidget.dart
+
 import 'package:flutter/material.dart';
 import 'package:test_project/theme/font.dart';
-
+import 'package:test_project/model/restaurant.dart';
 import '../storedetail/store_detail.dart';
 
 class PlaceWidget extends StatelessWidget {
-  final String name;
-  final String rating;
-  final String reviews;
-  final String distance;
-  final String image;
-  final String specificlocation;
-  final String foodtype;
-  final bool isFavorited;
+  final Restaurant restaurant;
   final VoidCallback onFavoriteToggle;
 
-  const PlaceWidget({
-    Key? key,
-    required this.name,
-    required this.rating,
-    required this.reviews,
-    required this.distance,
-    required this.image,
-    required this.specificlocation,
-    required this.foodtype,
-    required this.isFavorited,
-    required this.onFavoriteToggle,
-  }) : super(key: key);
+  PlaceWidget({required this.restaurant, required this.onFavoriteToggle});
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +22,7 @@ class PlaceWidget extends StatelessWidget {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => StoreDetailPage(name: name), // Pass necessary parameters
+              builder: (context) => StoreDetailPage(name: restaurant.name), // Pass necessary parameters
             ),
           );
         },
@@ -52,7 +36,7 @@ class PlaceWidget extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(12.0),
                   child: Image.asset(
-                    image,
+                    restaurant.image,
                     fit: BoxFit.cover,
                     width: 84,
                     height: 84,
@@ -64,22 +48,22 @@ class PlaceWidget extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(name, style: pretendardBold(context)),
+                    Text(restaurant.name, style: pretendardBold(context)),
                     Row(
                       children: [
                         Icon(Icons.star_rounded, color: Colors.yellow[700], size: 16),
-                        Text(rating, style: pretendardBold(context).copyWith(fontSize: 12)),
-                        Text(' ($reviews)', style: pretendardSemiBoldSecond(context).copyWith(fontSize: 12)),
+                        Text(restaurant.rating, style: pretendardBold(context).copyWith(fontSize: 12)),
+                        Text(' (${restaurant.reviews})', style: pretendardSemiBoldSecond(context).copyWith(fontSize: 12)),
                       ],
                     ),
-                    Text('$foodtype • $specificlocation • $distance', style: pretendardSemiBoldSecond(context).copyWith(fontSize: 12)),
+                    Text('${restaurant.foodtype} • ${restaurant.specificlocation} • ${restaurant.distance}', style: pretendardSemiBoldSecond(context).copyWith(fontSize: 12)),
                   ],
                 ),
               ),
               IconButton(
                 icon: Icon(
-                  isFavorited ? Icons.favorite : Icons.favorite_border,
-                  color: isFavorited ? Theme.of(context).colorScheme.primary : Colors.grey,
+                  restaurant.isFavorited ? Icons.favorite : Icons.favorite_border,
+                  color: restaurant.isFavorited ? Theme.of(context).colorScheme.primary : Colors.grey,
                 ),
                 onPressed: onFavoriteToggle,
               ),
---
+++ lib/model/restaurant.dart
@@ -0,0 +1,21 @@
+class Restaurant{
+  final String name;
+  final String rating;
+  final String reviews;
+  final String distance;
+  final String image;
+  final String specificlocation;
+  final String foodtype;
+  final bool isFavorited;
+
+  Restaurant({
+    required this.name,
+    required this.rating,
+    required this.reviews,
+    required this.distance,
+    required this.image,
+    required this.specificlocation,
+    required this.foodtype,
+    required this.isFavorited,
+  });
+}
